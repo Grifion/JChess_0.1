e9b36026f41f88e2de7afde365e0c4fbdcbd78be
Heorhii Lytvynenko <litvinenkogossha2@gmail.com>
2023-10-16 16:42:25 +0200

    First class Tile, all logic of Tiles

--- src/main/java/com/chess/engine/pieces/Piece.java
+++ src/main/java/com/chess/engine/pieces/Piece.java
@@ -1,4 +1,20 @@
 package  com.chess.engine.pieces;
-public class Piece {
 
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import java.util.List;
+
+public abstract class Piece {
+
+    protected final int peacePosition;
+
+    protected final Alliance piaceAlliance;
+
+
+    Piece(final int peacePosition, final Alliance piaceAlliance){
+        this.peacePosition = peacePosition;
+        this.piaceAlliance = piaceAlliance;
+
+    }
+    public abstract List<Move> calculateLegalMoves(final Board board);
 }
